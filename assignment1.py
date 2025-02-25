92612e1ae360b607eb9e931cdb61c55c882b5877
4rwen <1murphyarw@hdsb.ca>
2025-02-25 14:27:42 -0500

    Added variables and comments

--- assignment1.py
+++ assignment1.py
@@ -1,3 +1,23 @@
 #Author: Arwen Murphy
 #Date Modified: 25/02/25
 #Description: 
+
+int=1
+print("An example of a integer is: ",int)
+#An int (integer) is a whole number, that can be positive or negative, up to two billion.
+
+float=2.57
+print("An example of a float is: ",float)
+#A float is any number with a decimal. 
+#Doubles are similar to floats, but can only have one character after the decimal. (ex. 2.5)
+
+char="A"
+print("An example of a char is: ",char)
+#A char is a single character. It can be a letter (ex. A), number (ex. 1), or symbol (ex. !)
+
+booleans=True
+print("An example of boolean is: ",booleans)
+#A boolean is 
+
+str="hello world!"
+print("An example of a string is: ",str)
